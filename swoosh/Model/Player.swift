4a8bc4036cb56dceb3abe70e3fde321d44b43c2e
Ryan Scott <williamscott0511@gmail.com>
2020-06-02 09:43:51 -0700

    Lesson7
    Built some struct logic

---
+++ swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by William Scott on 6/2/20.
+//  Copyright © 2020 William Scott. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
