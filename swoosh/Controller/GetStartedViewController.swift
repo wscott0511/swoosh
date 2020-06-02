cb6cf6fa2782b4fef79663c4bed456e542af48d7
Ryan Scott <williamscott0511@gmail.com>
2020-06-02 09:26:01 -0700

    Lesson6
    More Segues

--- swoosh/Controller/GetStartedViewController.swift
+++ swoosh/Controller/GetStartedViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class GetStartedViewController: UIViewController {
     
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- swoosh/Controller/LeagueVC.swift
+++ swoosh/Controller/LeagueVC.swift
@@ -16,14 +16,18 @@ class LeagueVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
 
     /*
-    // MARK: - Navigation
+     MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+         Get the new view controller using segue.destination.
+         Pass the selected object to the new view controller.
     }
     */
 
